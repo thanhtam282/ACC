02023b8859c4ec07b1997751f2bfcc2362bbf2cd
Pisces <>
2019-02-27 10:31:53 +0700

    update product page

--- _components/_header/index.js
+++ _components/_header/index.js
@@ -8,7 +8,17 @@ const header = {
 	menuToggle: () => {
 		$('.acc-header .toggle-menu').on('click', function(){
 			$('.acc-header .bottom-nav').toggleClass('active')
+			$(this).toggleClass('active')
+			
 		})
+		// if ( $('.acc-header .bottom-nav.active').length  ) {
+		// 	console.log(1);
+		// 	$(document).click(function () { 
+				
+		// 		$('.acc-header .bottom-nav').removeClass('active')
+		// 	});
+		// }
+
 	},
 	headerInit: function(){
 		header.searchToggle();
