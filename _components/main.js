8b85bf0d424cec5cfabfd6ef9d7d2ac16d8598b1
ps06347 <vanhoangbao@gmail.com>
2019-02-28 15:08:01 +0700

    update about page

--- _components/main.js
+++ _components/main.js
@@ -8,6 +8,54 @@ $(document).ready(() => {
 	$(".acc-video-1 .video-wrapper").lightGallery({
 		mode: 'lg-scale-up'
 	});
+
+
+	var yearArr = []
+	$('[data-year]').each(function () {
+		yearArr.push($(this).data('year'))
+	})
+	let history_slider = new Swiper('.acc-about-2 .swiper-container', {
+		slidesPerView: 1,
+		autoHeight: false,
+		loop: true,
+		pagination: {
+			el: '.acc-about-2 .swiper-history-pagination',
+			clickable: true,
+			bulletClass: 'history-year',
+			bulletActiveClass: 'history-year-active',
+			renderBullet: function (index, className) {
+				return '<span class="' + className + '">' + (yearArr[index]) + '</span>';
+			},
+		},
+		navigation: {
+			nextEl: ".history-timeline .swiper-next",
+			prevEl: ".history-timeline .swiper-prev"
+		}
+	})
+
+	var previousActiveTabIndex = 0;
+
+	$(".tab-list li").on('click', function () {
+
+		var tabClicked = $(this).data("tab-index");
+
+		if (tabClicked != previousActiveTabIndex) {
+			$(this).addClass('active')
+			$(this).siblings().removeClass('active')
+			$(".panel-list .tab-container").each(function () {
+
+				if ($(this).data("tab-index") == tabClicked) {
+
+					$(".tab-container").removeClass('show');
+					$(this).addClass('show');
+					previousActiveTabIndex = $(this).data("tab-index");
+					return;
+
+				}
+			});
+		}
+	});
+
 	objectFitImages();
 	// Lĩnh vực
 	AOS.init();
@@ -49,9 +97,39 @@ var slider = {
 			},
 		})
 	},
+	relationship_slider_1: () => {
+		let relationship_slider = new Swiper('.acc-about-5 .swiper-container', {
+			slidesPerView: 6,
+			autoHeight: false,
+			loop: true,
+			spaceBetween: 30,
+			autoplay: {
+				delay: 5000,
+				disableOnInteraction: false,
+			},
+			navigation: {
+				nextEl: '.acc-about-5 .swiper-next',
+				prevEl: '.acc-about-5 .swiper-prev'
+			},
+			breakpoints: {
+				992: {
+					slidesPerView: 4
+				},
+				768: {
+					slidesPerView: 3
+				},
+				450: {
+					slidesPerView: 2
+				},
+				375: {
+					slidesPerView: 1
+				}
+			}
+		})
+	},
 	sliderInit: () => {
 		slider.product_slider_1();
 		slider.service_slider_1();
-
+		slider.relationship_slider_1();
 	}
 }
